f49eeadc9cfd15bb982cbb17a20df60c7b64a5f2
k9956245 <89276559+k9956245@users.noreply.github.com>
2026-09-03 21:58:06 +0000

    Change sweep strategy to hold R * CPR = NHWCPUS constant. Old sweep strategy was far too time consuming

--- hpcg_single_node_sweep.sh
+++ hpcg_single_node_sweep.sh
@@ -2,9 +2,11 @@
 #SBATCH --job-name=hpcgSNS
 #SBATCH --exclusive
 #SBATCH --nodes=1
-# Must request highest CPU count we intend to use upfront
-#SBATCH --ntasks=96
-#SBATCH --time=168:00:00
+# Must request highest CPU count we intend to use upfront for SLURM normal
+# partition sweep
+#SBATCH --ntasks=48
+#SBATCH --hint=nomultithread
+#SBATCH --time=48:00:00
 #SBATCH --nvram-options=none
 
 set -euo pipefail
@@ -15,6 +17,43 @@ module load compiler/2023.0.0
 module load mkl/2023.0.0
 module load mpi/2021.15
 
+# The implementation decison logic can definitely be improved
+IMPLEMENTATION="${1:-intel}"
+declare -a R_LIST
+
+detect_system() {
+    local ncpu
+    ncpu=$(lscpu | awk '/^CPU\(s\):/ {print $NF}')
+
+    # gnr has model name "Genuine Intel(R) 0000" so better to use logical core
+    # counts instead. c is physical core count
+    case "$ncpu" in
+        128)
+            IMPLEMENTATION=amd
+            R_LIST=(1 2 4 8 16 32 64)
+            printf 'amd\n' ;; # 2x EPYC 7502, 64c SMT-2
+        112)
+            R_LIST=(1 2 4 7 8 14 28 56)
+            printf 'icx\n' ;; # 2x Xeon Gold 6330, 56c SMT-2
+        480)
+            R_LIST=(1 2 3 4 6 8 12 16 24 32 48 64 120 240)
+            RUNNING_ON_GNR="${RUNNING_ON_GNR:-1}"
+            printf 'gnr\n' ;; # 2x Xeon 6979P eng sample, 240c SMT-2
+        96)
+            R_LIST=(1 2 4 6 8 12 16 24 32 48)
+            printf 'normal\n' ;; # 2x Xeon 8268, 48c SMT-2
+        *)
+            printf 'ERROR: unrecognised system (CPUs=%s)\n' "$ncpu" >&2
+            return 1
+            ;;
+    esac
+
+    N_HW_CPU=$((ncpu / 2))
+}
+detect_system
+
+IMPLEMENTATION="${IMPLEMENTATION,,}"
+
 # Set to 1 if you are testing on Granite Rapids
 RUNNING_ON_GNR="${RUNNING_ON_GNR:-0}"
 RUNNER="${RUNNER:-auto}" # auto, slurm, or mpi
@@ -22,9 +61,8 @@ RUNNER="${RUNNER:-auto}" # auto, slurm, or mpi
 # large problem sizes. This will however mean the problem size may not meet
 # the 25% memory capacity requirement for a fair test
 # I added this because I had difficulty compiling AMD's HPCG with 64-bit
-# global indices, and the problem size on the normal build was pretty close
-# to 25%
-CLAMP_PROB_SIZE="${CLAMP_PROB_SIZE:-0}"
+# global indices. This does not adequately fill gnr's memory capacity
+CLAMP_PROB_SIZE="${CLAMP_PROB_SIZE:-1}"
 
 INTEL_HPCG_PATH="$HOME/benchmarks/intel_hpcg/bin/xhpcg_skx"
 INTEL_ILP64_HPCG_PATH="$HOME/benchmarks/intel_hpcg_ilp64/bin/xhpcg_skx_ilp64"
@@ -32,7 +70,9 @@ INTEL_ILP64_HPCG_PATH="$HOME/benchmarks/intel_hpcg_ilp64/bin/xhpcg_skx_ilp64"
 # This can be found in the 'Memory Use Summary'section of a HPCG output, run it
 # for any problem size which is a multiple of 8 and > 24 and round bytes per
 # equation to the nearest integer
-BYTES_PER_EQUATION="${BYTES_PER_EQUATION:-715}"
+BPE_NORMAL=715
+BPE_ILP64=839
+BYTES_PER_EQUATION="${BYTES_PER_EQUATION:-$BPE_NORMAL}"
 HPCG_RUN_TIME="${HPCG_RUN_TIME:-60}"
 
 usage() {
@@ -44,9 +84,6 @@ if (( $# > 1 )); then
     usage
 fi
 
-IMPLEMENTATION="${1:-intel}"
-IMPLEMENTATION="${IMPLEMENTATION,,}"
-
 declare -a SLURM_LAUNCH_ARGS
 
 case "$IMPLEMENTATION" in
@@ -55,8 +92,8 @@ case "$IMPLEMENTATION" in
         ;;
     intel_ilp64)
         [[ -v XHPCG_BIN ]] || XHPCG_BIN="$INTEL_ILP64_HPCG_PATH"
-        if (( BYTES_PER_EQUATION == 715 )); then
-            BYTES_PER_EQUATION=839
+        if (( BYTES_PER_EQUATION == BPE_NORMAL )); then
+            BYTES_PER_EQUATION="$BPE_ILP64"
         fi
         ;;
     amd)
@@ -75,9 +112,6 @@ case "$IMPLEMENTATION" in
         ;;
 esac
 
-N_PROC=$(nproc)
-MAX_TOTAL_CPUS="${MAX_TOTAL_CPUS:-${SLURM_NTASKS:-$N_PROC}}"
-MAX_TOTAL_THREADS="$MAX_TOTAL_CPUS"
 # The code to set the problem size sometimes undershoots, so use 17/64 (~26.5%) 
 # instead of 1/4. I use 272 since (17/64)*1024=272 is an integer
 MEM_TARGET_BYTES=$(grep MemTotal /proc/meminfo | awk '{print $2 * 272}')
@@ -139,7 +173,7 @@ round_mult8() {
     }'
 }
 
-# Override mpi_configure_default and do nothing
+# Override mpi_configure_default and do nothing (we build MPI_ARGS ourselves)
 mpi_configure_user() {
     true
 }
@@ -173,16 +207,16 @@ HST=$(hostname -s)
 # From hpcg_common.sh
 RUN_ROOT=$(print_run_root "${SLURM_SUBMIT_DIR:-$PWD}" "${HST}_s_")
 
-FAILED_LOG="$RUN_ROOT/failed_runs.log"
+FAILED_LOG="${RUN_ROOT:-.}/failed_runs.log"
 
 if [[ "$CLAMP_PROB_SIZE" -gt 0 || "$CLAMP_PROB_SIZE" == 'true'  ]]; then
-    printf 'CLAMP_PROB_SIZE is ON. Problem size limited to 424^3\n'
+    printf '\nCLAMP_PROB_SIZE is ON. Problem size limited to 424^3\n'
 fi
 
 run_benchmark() {
     local ntasks="$1"
     local cpus_per_task="$2"
-    local threads_per_task="$3"
+    local threads_per_task="$cpus_per_task"
 
     local local_volume initial_side nx ny nz_initial nz run_root run_dir
     # We need to scale the local problem size so that it uses ~25% total
@@ -211,7 +245,7 @@ run_benchmark() {
     
     SLURM_LAUNCH_ARGS=(
         --mem-bind=local
-        --cpu-bind='verbose,ldoms'
+        --cpu-bind='verbose,cores'
         --ntasks="$ntasks"
         --cpus-per-task="$cpus_per_task"
     )
@@ -222,7 +256,7 @@ run_benchmark() {
     MPI_ARGS=()
     case "$MPI_IMPL" in
         openmpi)
-            MPI_ARGS+=(--host localhost)
+            MPI_ARGS+=(--host localhost:"$ntasks")
             MPI_ARGS+=(-np "$ntasks")
             MPI_ARGS+=(--map-by slot:PE="$cpus_per_task":HWTCPUS)
             MPI_ARGS+=(--bind-to hwthread)
@@ -237,8 +271,11 @@ run_benchmark() {
             MPI_ARGS+=(-hosts localhost)
             MPI_ARGS+=(-n "$ntasks")
             MPI_ARGS+=(-genv I_MPI_PIN 1)
-            MPI_ARGS+=(-genv I_MPI_PIN_CELL unit)
+            MPI_ARGS+=(-genv I_MPI_PIN_CELL core)
             MPI_ARGS+=(-genv I_MPI_PIN_DOMAIN "$cpus_per_task")
+            MPI_ARGS+=(-genv I_MPI_PIN_ORDER compact)
+            MPI_ARGS+=(-genv I_MPI_DEBUG 5)
+            MPI_ARGS+=(-genv KMP_AFFINITY granularity='fine,scatter')
             MPI_ARGS+=(-genvlist 'FI_PROVIDER,OMP_NUM_THREADS,MKL_NUM_THREADS')
             if (( RUNNING_ON_GNR )); then
                 MPI_ARGS+=(-genv I_MPI_FABRICS shm)
@@ -254,5 +291,14 @@ run_benchmark() {
         run "$BINARY_PATH"
     )
 }
-# From sweep_common.sh
-run_sweep
+
+for R in "${R_LIST[@]}"; do
+    CPR=$((N_HW_CPU / R))
+    if ! run_benchmark "$R" "$CPR"; then
+        printf 'WARNING: run FAILED: ntasks=%u cpus_per_task=%u (see %s)' \
+            "$R" "$CPR" "$FAILED_LOG"
+        printf '%s ntasks=%u cpus_per_task=%u\n' \
+                "$(date -u +%Y-%m-%dT%H%M%SZ)" "$R" "$CPR" \
+                >> "$FAILED_LOG"
+    fi
+done
